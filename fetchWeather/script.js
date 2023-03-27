cc69a7f71ed714f2c6973c8c329db0e0f2c68225
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-27 17:17:28 -0500

    added event listener

--- fetchWeather/script.js
+++ fetchWeather/script.js
@@ -1,14 +1,14 @@
 // Eventually this will be replaced with a element.value in a Input tag
 const apiKey = "011ceaa673e608d33292e3f8d37fa170";
-let cityname = "Birmingham";
-let state = "AL";
-let countryCode = "US"
+let cityname = document.getElementById("city") ;
+let state = document.getElementById("state");
+let countryCode = document.getElementById("country");
 const limit = "5"
 let lan =""
 let lon = ""
 let currentWeatherURL = ""
-let geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityname},${state},${countryCode}&limit=${limit}&appid=${apiKey}`
 async function getCityWeather(){
+    let geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityname.value},${state.value},${countryCode.value}&limit=${limit}&appid=${apiKey}`
     let res = await fetch(geoURL)
     let data = await res.json()
     let dataObj = data[0]
@@ -17,21 +17,32 @@ async function getCityWeather(){
     lon = dataObj.lon
     let st8 = dataObj.state
     let country = dataObj.country
-    currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    // currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
     let cityEl = document.getElementById("cityName");
     cityEl.textContent = city;
-    console.log(city, lat, lon, st8, country) 
-    getCurrentWeather()
-}
-getCityWeather()
-
-async function getCurrentWeather(){
-    let res = await fetch(currentWeatherURL)
-    let data = await res.json()
-    let dataObj = data
-    console.log(dataObj)
+    let stateEl = document.getElementById("stateName");
+    stateEl.textContent = st8;
+    let countryEl = document.getElementById("countryName");
+    countryEl.textContent = country;
+    console.log(dataObj) 
     let day1Icon = dataObj.weather[0].icon
     let iconURL = `https://openweathermap.org/img/wn/${day1Icon}@2x.png`
     let day1IconID = document.getElementById("day1Icon")
     day1IconID.src = iconURL
 }
+getCityWeather()
+
+// async function getCurrentWeather(){
+//     let res = await fetch(geoURL, { mode: "cors" })
+//     let data = await res.json()
+//     let dataObj = data
+//     console.log(dataObj)
+// }
+
+let formSubmit = document.getElementById("form")
+formSubmit.addEventListener("submit", (event) => {
+    event.preventDefault()
+    console.log("Test")
+    getCityWeather()
+})
+console.log(formSubmit)
