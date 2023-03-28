5aeeaca08c7f0ce6a0757eeb1e6596b7b9bcd559
Ben Bryant <brbryant2639@gmail.com>
2023-03-27 19:05:31 -0500

    UPDATE: icon fix and temp display

--- fetchWeather/script.js
+++ fetchWeather/script.js
@@ -2,55 +2,53 @@
 const apiKey = "011ceaa673e608d33292e3f8d37fa170";
 
 // dom elements
-let cityEle = document.getElementById("city") ;
+let cityEle = document.getElementById("city");
 let stateEle = document.getElementById("state");
 let countryEle = document.getElementById("country");
 let formEle = document.getElementById("form");
+let weatherDisplayEle = document.getElementById("weatherStatusDay1");
 
 // state
-const limit = "5"
-let lan =""
-let lon = ""
-let currentWeatherURL = ""
+const limit = "5";
+let lan = "";
+let lon = "";
+let currentWeatherURL = "";
 
-async function getCityWeather(){
-    let geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityEle.value},${stateEle.value},${countryEle.value}&limit=${limit}&appid=${apiKey}`
-    let res = await fetch(geoURL, { mode: "cors" })
-    let data = await res.json()
-    let dataObj = data[0]
-    lat = dataObj.lat
-    lon = dataObj.lon
-    let city = dataObj.name
-    let st8 = dataObj.state
-    let country = dataObj.country
-    currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
-    
-    // updates the dom
-    let cityEl = document.getElementById("cityName");
-    cityEl.textContent = city;
-    let stateEl = document.getElementById("stateName");
-    stateEl.textContent = st8;
-    let countryEl = document.getElementById("countryName");
-    countryEl.textContent = country;
-    
-    let day1Icon = dataObj.weather[0].icon
-    // todo: check icon source
-    let iconURL = `https://openweathermap.org/img/wn/${day1Icon}@2x.png`
-    let day1IconID = document.getElementById("day1Icon")
-    day1IconID.src = iconURL
-    
-    getCurrentWeather();
+async function getCityWeather() {
+  let geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityEle.value},${stateEle.value},${countryEle.value}&limit=${limit}&appid=${apiKey}`;
+  let res = await fetch(geoURL, { mode: "cors" });
+  let data = await res.json();
+  let dataObj = data[0];
+  lat = dataObj.lat;
+  lon = dataObj.lon;
+  let city = dataObj.name;
+  let st8 = dataObj.state;
+  let country = dataObj.country;
+  currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+
+  // updates the dom
+  let cityEl = document.getElementById("cityName");
+  cityEl.textContent = city;
+  let stateEl = document.getElementById("stateName");
+  stateEl.textContent = st8;
+  let countryEl = document.getElementById("countryName");
+  countryEl.textContent = country;
+  getCurrentWeather();
 }
 
-async function getCurrentWeather(){
-    let res = await fetch(currentWeatherURL, { mode: "cors" })
-    let data = await res.json()
-    console.log(data)
-    let dataObj = data;
-    // todo: display weather data to screen...
+async function getCurrentWeather() {
+  let res = await fetch(currentWeatherURL, { mode: "cors" });
+  let data = await res.json();
+  console.log(data);
+  let dataObj = data;
+  weatherDisplayEle.textContent = `${Math.round(dataObj.main.temp)} °F`;
+  let day1Icon = dataObj.weather[0].icon;
+  let iconURL = `https://openweathermap.org/img/wn/${day1Icon}@2x.png`;
+  let day1IconID = document.getElementById("day1Icon");
+  day1IconID.src = iconURL;
 }
 
 formEle.addEventListener("submit", (event) => {
-    event.preventDefault();
-    getCityWeather();
-})
+  event.preventDefault();
+  getCityWeather();
+});
